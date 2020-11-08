bd7a6a631bfead049f92d77694db294071e6d95b
Khoa Le <43885032+khoale9098@users.noreply.github.com>
2020-11-09 00:16:27 +0700

    fix tailwind

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -2,19 +2,9 @@ module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   future: {
     removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
   },
-  theme: {
-    backgroundColor: (theme) => ({
-      ...theme('colors'),
-      primary: '#5369f8',
-      secondary: '#1e2139',
-      success: '#43d39e',
-      info: '#25c2e3',
-      warning: '#ffbe0b',
-      danger: '#ff5c75',
-    }),
+  variants: {
+    width: ['responsive', 'hover', 'focus'],
   },
-  variants: {},
   plugins: [],
 }
