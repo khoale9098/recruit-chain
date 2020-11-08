d1045abe92dfe1efa8bd19a7b4c16b8ea28c90cf
Khoa Le <43885032+khoale9098@users.noreply.github.com>
2020-11-09 00:35:24 +0700

    add

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -1,11 +1,25 @@
 module.exports = {
-  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
-  important: true,
   future: {
     removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
   },
-  variants: {
-    width: ['responsive', 'hover', 'focus'],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    backgroundColor: (theme) => ({
+      ...theme('colors'),
+      primary: '#5369f8',
+      secondary: '#1e2139',
+      success: '#43d39e',
+      info: '#25c2e3',
+      warning: '#ffbe0b',
+      danger: '#ff5c75',
+    }),
   },
+  variants: {},
   plugins: [],
 }
